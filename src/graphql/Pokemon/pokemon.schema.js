5376b5fc11f7080d815337a9071c5b6f90d5b6ca
Tyler Hall <0tillathehun0@gmail.com>
2021-11-08 02:26:43 +0000

    feat: add name and favorite resolvers

--- src/graphql/Pokemon/pokemon.schema.js
+++ src/graphql/Pokemon/pokemon.schema.js
@@ -13,6 +13,14 @@ export const typeDefs = gql`
 
 export const resolvers = {
   Pokemon: {
+    name: async (pokemon) => {
+      const { name } = pokemon
+      return name.charAt(0).toUpperCase() + name.slice(1)
+    },
+    favorite: async (pokemon, _args, { apis: { pokeApi } }) => {
+      const res = await pokeApi.findPokemonMetaByName(pokemon.name)
+      return !!(res && res.favorite)
+    },
     moves: async (pokemon, _args, { apis: { pokeApi } }) => {
       console.log('fetching moves')
       const moves = await pokeApi.findPokemonMoves(pokemon.name)
